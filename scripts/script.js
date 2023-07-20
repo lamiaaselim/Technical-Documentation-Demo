23d977363da14ddfeba34e40eb5b372686a09033
Lamiaa Selim <lamiaaselim1896@gmail.com>
2023-07-20 21:24:39 +0300

    upload project

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+// // Technical Documentation Demo
+
+
+
+
+
+// // Nice Scrolling
+// $(document).ready(function () {
+// 	"use strict";
+// $("html").niceScroll({
+//     cursorcolor:"#140606",
+// 	cursorwidth:"12px",
+// 	cursorborder:"#E41B17 solid 2px"
+// });
+// });
